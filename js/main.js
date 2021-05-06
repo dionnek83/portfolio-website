07b04e7fe899f64b01da56853dcf979d79d63b13
dionnek83 <dionnek83@gmail.com>
2021-05-06 20:17:20 +0200

    Modified a little of bit of everything

--- js/main.js
+++ js/main.js
@@ -1,50 +1,47 @@
-// define all UI variable
-const navToggler = document.querySelector('.nav-toggler');
-const navMenu = document.querySelector('.site-navbar ul');
-const navLinks = document.querySelectorAll('.site-navbar a');
+const navToggler = document.querySelector(".nav-toggler");
+const navMenu = document.querySelector(".site-navbar ul");
+const navLinks = document.querySelectorAll(".site-navbar a");
 
-// load all event listners
 allEventListners();
 
-// functions of all event listners
 function allEventListners() {
-  // toggler icon click event
-  navToggler.addEventListener('click', togglerClick);
-  // nav links click event
-  navLinks.forEach( elem => elem.addEventListener('click', navLinkClick));
+  navToggler.addEventListener("click", togglerClick);
+  navLinks.forEach((elem) => elem.addEventListener("click", navLinkClick));
 }
 
-// togglerClick function
 function togglerClick() {
-  navToggler.classList.toggle('toggler-open');
-  navMenu.classList.toggle('open');
+  navToggler.classList.toggle("toggler-open");
+  navMenu.classList.toggle("open");
 }
 
-// navLinkClick function
 function navLinkClick() {
-  if(navMenu.classList.contains('open')) {
+  if (navMenu.classList.contains("open")) {
     navToggler.click();
   }
 }
 
-// ===================== SCROLL EFFECT ======================
-// const section = document.querySelectorAll('section[id]');
-// window.addEventListener('scroll' scrollActive)
+var form = document.getElementById("my-form");
 
-// function scrollActive(){
-//   const scrollY = window.pageXOffset
-
-//   SpeechRecognitionResult.forEach(current =>){
-//     const sectionHeight = current.offsetHeight
-//    const sectionTop = current.offsetTop - 50;
-//    sectionId =current.getAttribute('id')
-
-//    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-//      document.querySelector('.nav__menu a[href*=]' + sectionId + ']').classList.add('active')
-//    }
-//    else{
-//     document.querySelector('.nav__menu a[href*=]' + sectionId + ']').classList.remove('active')
-//    }
-//   }
-// }
-
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+      status.innerHTML =
+        "  <h3>Success!</h3> <p>Your message has been sent </p> ";
+      status.classList.add("success");
+      form.reset();
+    })
+    .catch((error) => {
+      status.innerHTML = "<h3>Error!</h3> <p>Your message was not sent</p>";
+      status.classList.add("error");
+    });
+}
+form.addEventListener("submit", handleSubmit);
